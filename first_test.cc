46f983bfc6b76f1be7cefa87019221aa90008121
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2020-12-21 15:22:42 +1100

    updates

--- first_test.cc
+++ first_test.cc
@@ -11,6 +11,21 @@ TF_LITE_MICRO_TEST(LoadModelAndPerformInference) {
 
 // MicroErrorReporter overrides a mthod on ErrorReporter
 // Create an ErrorReporter and point ot to micro_error_reporter
+// A pointer doesnt hold a variable,(*) it holds a place in memory where the value exists
+// User error_reporter for debugging
 
 tflite::MicroErrorReporter micro_error_reporter;
 tflite::ErrorReporter* error_reporter = &micro_error_reporter;
+
+// Map the model into a usable data structure. This doesn't involve any
+// copying or parsing, it's a very lightweight operation.
+
+// Get data array defined in simple_model, which returns a model pointer
+// This gets assigned to variable model == simple_model
+// The Model is a struct which is similar to a class in C++
+
+const tflite::Model* model = ::tflite::GetModel(g_sine_model_data);
+if (model->version() != TFLITE_SCHEMA_VERSION) { error_reporter->Report(
+"Model provided is schema version %d not equal " "to supported version %d.\n",
+model->version(), TFLITE_SCHEMA_VERSION); return 1;
+}
