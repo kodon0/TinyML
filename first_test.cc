bb26f9ace831bb588c858971ca53984921af796d
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2020-12-21 17:23:17 +1100

    finished pre-test

--- first_test.cc
+++ first_test.cc
@@ -125,3 +125,21 @@ float value = output->data.f[0];
 
 // Check that the output value is within 0.05 of the expected value. If pass, things are good
 TF_LITE_MICRO_EXPECT_NEAR(0., value, 0.05);
+
+// Run inference on several more values and confirm the expected outputs
+// Check if for example sin(3) is approx 0.141
+input->data.f[0] = 1.;
+interpreter.Invoke();
+value = output->data.f[0];
+TF_LITE_MICRO_EXPECT_NEAR(0.841, value, 0.05);
+input->data.f[0] = 3.;
+interpreter.Invoke();
+value = output->data.f[0];
+TF_LITE_MICRO_EXPECT_NEAR(0.141, value, 0.05);
+input->data.f[0] = 5.;
+interpreter.Invoke();
+value = output->data.f[0];
+TF_LITE_MICRO_EXPECT_NEAR(-0.959, value, 0.05);
+
+}
+TF_LITE_MICRO_TESTS_END
