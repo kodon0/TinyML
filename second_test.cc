e0d12b81b0fda85b0cb5218fa9524d2c47e89562
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2020-12-28 10:49:54 +1100

    Create second_test.cc
    added descriptions of logi, fucntions and correlations of TFlite for microcontrollers

---
+++ second_test.cc
@@ -0,0 +1,133 @@
+// IMPORTS...
+//
+// NOTE! Old version of this do NOT have quantization steps...
+
+#include "tensorflow/lite/micro/examples/hello_world/main_functions.h"
+
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/examples/hello_world/constants.h"
+#include "tensorflow/lite/micro/examples/hello_world/model.h"
+#include "tensorflow/lite/micro/examples/hello_world/output_handler.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+// New imports are constants.h and output_handler.h
+
+// Globals, used for compatibility with Arduino-style sketches.
+// These gloabs can only be used in this file
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+int inference_count = 0;
+
+constexpr int kTensorArenaSize = 2000;
+uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
+
+// DEFINE SETUP FUNCTION
+// Tensor arena is also defined here as before
+
+// The name of this function is important for Arduino compatibility.
+void setup() {
+  // Set up logging. Google style is to avoid globals or statics because of
+  // lifetime uncertainty, but since this has a trivial destructor it's okay.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(g_model);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  // This pulls in all the operation implementations we need.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::AllOpsResolver resolver;
+
+  // Build an interpreter to run the model with.
+  static tflite::MicroInterpreter static_interpreter(
+      model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
+
+  // Obtain pointers to the model's input and output tensors.
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+
+  // Keep track of how many inferences we have performed. - set to 0
+  inference_count = 0;
+}
+
+// DEFINE LOOP FUNCTION
+// First thing is to define what values to be passed into the model.
+// These are defined in constants.h and output_handler.h and are kXrange and kInferencesPerCycle
+// convention states that you put k in fron of statics
+//
+void loop() {
+  // Calculate an x value to feed into the model. We compare the current
+  // inference_count to the number of inferences per cycle to determine
+  // our position within the range of possible x values the model was
+  // trained on, and use this to calculate a value.
+  float position = static_cast<float>(inference_count) /
+                   static_cast<float>(kInferencesPerCycle);
+  float x = position * kXrange;
+  // This just divides the count by range so we know where we are. static_cast converts items to floats
+
+  // This next bit qunatizes the input, and writes the x value to model's input tensor
+
+  // Quantize the input from floating-point to integer
+  int8_t x_quantized = x / input->params.scale + input->params.zero_point;
+  // Place the quantized input in the model's input tensor
+  input->data.int8[0] = x_quantized;
+
+  // Run inference, and report any error
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed on x: %f\n",
+                         static_cast<double>(x));
+    return;
+  }
+
+// Grab result from output tensor (y value)
+  // Obtain the quantized output from model's output tensor
+  int8_t y_quantized = output->data.int8[0];
+  // Dequantize the output from integer to floating-point
+  float y = (y_quantized - output->params.zero_point) * output->params.scale;
+
+  // The output_handler.cc defines the Handleoutput() function
+
+  void HandleOutput(tflite::ErrorReporter* error_reporter, float x_value, float y_value) {
+      // Log the current X and Y values
+      error_reporter->Report("x_value: %f, y_value: %f\n", x_value, y_value); }
+
+  // For each microcontroller, we need to specify a different custom replacememnt for output_handler.cc
+  // Output the results. A custom HandleOutput function can be implemented
+// for each supported hardware target.
+HandleOutput(error_reporter, x, y);
+
+// Last bit is to increment the count by 1 for each loop
+// Increment the inference_counter, and reset it if we have reached
+// the total number per cycle
+inference_count += 1;
+if (inference_count >= kInferencesPerCycle) inference_count = 0;
+}
+
+// 
+// To make this loop run conttinually, the anser is is in main.cc
